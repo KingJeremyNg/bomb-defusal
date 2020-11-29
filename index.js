064f24d986359898dbd885a760811c3f47b33619
KingJeremyNg <jeremy97ng@gmail.com>
2020-11-28 21:29:14 -0500

    Added keypress event listener

---
+++ index.js
@@ -0,0 +1,32 @@
+const log = document.querySelector('input');
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+
+input.onkeydown = keyPress;
+
+var array = [];
+function keyPress(e) {
+    if (e.code == "Backspace") {
+        array.pop();
+    }
+    else if (e.code.startsWith("Key")) {
+        array.push(e.code.slice(-1));
+    }
+    else if (e.code == "Space") {
+        array.push(e.code);
+    }
+
+    console.log(array);
+    output.innerHTML = (`${array}`);
+}
+
+
+
+// (async () => {
+//     var input = document.getElementById("input");
+//     var output = document.getElementById("output");
+
+//     output.innerHTML = input.innerHTML;
+
+//     output.innerHTML = "THIS WORK??";
+// })

5cb4b6c33dd6dc509bb1ffa3b66ab89565cc13e8
KingJeremyNg <jeremy97ng@gmail.com>
2020-11-29 23:11:00 -0500

    Edit index.html and wire.js

---
+++ src/passwords.js
@@ -0,0 +1,8 @@
+const passwordsInput = document.getElementById('passwordsInput');
+const passwordsOutput = document.getElementById('passwordsOutput');
+
+passwordsInput.onkeydown = keyPress;
+
+function keyPress(e) {
+    // console.log(passwordsInput.value);
+}
--- src/wires.js
+++ src/wires.js
@@ -11,37 +11,37 @@ wiresInput6.addEventListener("click", mouseClick6);
 
 function mouseClick3() {
     wiresOutput.innerHTML = (
-        `1. If there are no red wires, cut the <strong>second</strong> wire.<br>
-        2. If the <strong>last</strong> wire is white, cut the <strong>last</strong> wire.<br>
-        3. If there is more than one blue wire, cut the <strong>last</strong> blue wire.<br>
-        4. Cut the <strong>last</strong> wire.`
+        `<li>If there are <strong>no</strong> red wires, cut the <strong>second</strong> wire.</li>
+        <li>If the <strong>last</strong> wire is white, cut the <strong>last</strong> wire.</li>
+        <li>If there is <strong>more than one</strong> blue wire, cut the <strong>last</strong> blue wire.</li>
+        <li>Cut the <strong>last</strong> wire.</li>`
     );
 }
 
 function mouseClick4() {
     wiresOutput.innerHTML = (
-        `1. If there is <strong>more than one</strong> red wire and the <strong>last digit</strong> of the serial number is <strong>odd</strong>, cut the <strong>last</strong> red wire.<br>
-        2. If the <strong>last</strong> wire is yellow and there are <strong>no</strong> red wires, cut the <strong>first</strong> wire.<br>
-        3. If there is <strong>exactly one</strong> blue wire, cut the <strong>first</strong> wire.<br>
-        4. If there is <strong>more than one</strong> yellow wire, cut the <strong>last</strong> wire.<br>
-        5. Cut the <strong>second</strong> wire.`
+        `<li>If there is <strong>more than one</strong> red wire and the <strong>last digit</strong> of the serial number is <strong>odd</strong>, cut the <strong>last</strong> red wire.</li>
+        <li>If the <strong>last</strong> wire is yellow and there are <strong>no</strong> red wires, cut the <strong>first</strong> wire.</li>
+        <li>If there is <strong>exactly one</strong> blue wire, cut the <strong>first</strong> wire.</li>
+        <li>If there is <strong>more than one</strong> yellow wire, cut the <strong>last</strong> wire.</li>
+        <li>Cut the <strong>second</strong> wire.</li>`
     );
 }
 
 function mouseClick5() {
     wiresOutput.innerHTML = (
-        `1. If the <strong>last</strong> wire is black and the <strong>last digit</strong> of the serial number is <strong>odd</strong>, cut the <strong>fourth</strong> wire.<br>
-        2. If there is <strong>exactly one</strong> red wire and there is <strong>more than one</strong> yellow wire, cut the <strong>first</strong> wire.<br>
-        3. If there are <strong>no</strong> black wires, cut the <strong>second</strong> wire.<br>
-        4. Cut the <strong>first</strong> wire.`
+        `<li>If the <strong>last</strong> wire is black and the <strong>last digit</strong> of the serial number is <strong>odd</strong>, cut the <strong>fourth</strong> wire.</li>
+        <li>If there is <strong>exactly one</strong> red wire and there is <strong>more than one</strong> yellow wire, cut the <strong>first</strong> wire.</li>
+        <li>If there are <strong>no</strong> black wires, cut the <strong>second</strong> wire.</li>
+        <li>Cut the <strong>first</strong> wire.</li>`
     );
 }
 
 function mouseClick6() {
     wiresOutput.innerHTML = (
-        `1. If there are <strong>no</strong> yellow wires and the <strong>last digit</strong> of the serial number is <strong>odd</strong>, cut the <strong>third</strong> wire.<br>
-        2. If there is <strong>exactly one</strong> yellow wire and there is <strong>more than one</strong> white wire, cut the <strong>fourth</strong> wire.<br>
-        3. If there are <strong>no</strong> red wires, cut the <strong>last</strong> wire.<br>
-        4. Cut the <strong>fourth</strong> wire.`
+        `<li>If there are <strong>no</strong> yellow wires and the <strong>last digit</strong> of the serial number is <strong>odd</strong>, cut the <strong>third</strong> wire.</li>
+        <li>If there is <strong>exactly one</strong> yellow wire and there is <strong>more than one</strong> white wire, cut the <strong>fourth</strong> wire.</li>
+        <li>If there are <strong>no</strong> red wires, cut the <strong>last</strong> wire.</li>
+        <li>Cut the <strong>fourth</strong> wire.</li>`
     );
 }
